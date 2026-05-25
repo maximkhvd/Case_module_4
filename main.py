c89c99911b17a17f95ef339965dab0a0cf2cf90a
maximkhvd <maximkhvd@gmail.com>
2026-05-25 19:38:11 +0300

    Update main.py
    Реализована функция add_book.

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+def add_book(books):
+    numbers = [1, 2, 3, 4, 5]
+    """Добавление новой книги"""
+    print("\nДобавление новой книги:")
+    author = input("Введите имя автора: ").strip()
+    name = input("Введите назвние книги: ").strip()
+    rating = int(input("Введите оценку книги от 1 до 5: ").strip())
+    while not rating in numbers:
+        rating = int(input("Введите оценку книги от 1 до 5: ").strip())
+    time = input("Введите дату создания книги: ").strip()
+    book = {"author": author,
+            "name": name,
+            "rating": rating, 
+            "time": time
+            }
+    books.append(book)
+    save_books(books)
+    print("Книга успешно добавлена!")
+
 def load_books():
     pass
 
